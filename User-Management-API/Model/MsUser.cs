4d26a8614a7312dda8818a343f89a19f9a7ddc1e
Steven Forsythia <stvnhd164@gmail.com>
2023-04-04 12:49:18 +0700

    feat: add helper, controller, model

---
+++ User-Management-API/Model/MsUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace User_Management_API.Model
+{
+    public class MsUser
+    {
+        [Key] public BigInteger staffId { get; set; }
+        public string staffName { get; set; }
+        public char gender { get; set; }
+        public string dob { get; set; }
+        public string posisition { get; set; }
+        public string email { get; set; }
+        public string addres { get; set; }
+    }
+}
